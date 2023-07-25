c9e2d2c585b1def23598c3a842df736761ac7635
sseezov <sseezov@mail.ru>
2023-07-25 21:03:20 +0700

    step 2 done badly

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ import Validator from './src/Validator.js';
 const v = new Validator();
 
 const schema = v.string();
+const num = v.number();
 
 console.log(schema.isValid(''));
 console.log(schema.isValid(null));
 console.log(schema.isValid(undefined));
+console.log(num.isValid(null));
+console.log('after required');
 schema.required();
+console.log(num.isValid(null));
+console.log(num.isValid(7));
+console.log(num.positive().isValid(7));
+console.log(1);
+
+num.range(-5, 5);
+console.log(num.isValid(-3));
+console.log(num.isValid(5));
+console.log(2);
 console.log(schema.isValid('what does the fox say'));
+console.log(schema.isValid('what does the fox say'));
+
 console.log(schema.isValid('hexlet'));
 console.log(schema.isValid(null));
 console.log(schema.isValid(''));
 console.log(schema.contains('what').isValid('what does the fox say'));
 console.log(schema.contains('whatthe').isValid('what does the fox say')); // false);
 console.log(schema.isValid('what does the fox say')); // false); // false);
+
 // console.log(schema.isValid(''));
 // console.log(schema.isValid(''));
 // console.log(schema.isValid(''));
--- src/Validator.js
+++ src/Validator.js
@@ -1,11 +1,11 @@
 export default class Validator {
   constructor() {
+    this.isRequired = false;
     this.string = function string() {
       return {
         shoudContain: [],
-        isRequired: false,
         isValid: function isValid(value) {
-          if (!this.isRequired) {
+          if (!Validator.isRequired) {
             return true;
           }
           if (this.shoudContain.length === 0) {
@@ -15,11 +15,35 @@ export default class Validator {
           return contains.length === this.shoudContain.length && !!value;
         },
         required: function required() {
-          this.isRequired = !this.isRequired;
+          Validator.isRequired = !Validator.isRequired;
         },
         contains: function contains(value) {
           this.shoudContain.push(value);
           return this;
+        },
+      };
+    };
+    this.number = function number() {
+      return {
+        isValid: function isValid(value) {
+          if (!Validator.isRequired) {
+            return true;
+          } if (this.ranges.length > 0) {
+            if (this.isPositive === true) {
+              return typeof value === 'number' && value >= 0
+                && value >= this.ranges[0] && value <= this.ranges[1];
+            }
+            return typeof value === 'number' && value >= this.ranges[0] && value <= this.ranges[1];
+          } return typeof value === 'number';
+        },
+        isPositive: false,
+        positive: function positive() {
+          this.isPositive = true;
+          return this;
+        },
+        ranges: [],
+        range: function range(start, end) {
+          this.ranges = [start, end];
         },
       };
     };
